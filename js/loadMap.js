5d8ace158914ede6c177dacfa14e521986ea25b0
moyo <moriyori10@gmail.com>
2025-08-26 19:32:12 +0900

    next up 3

--- js/loadMap.js
+++ js/loadMap.js
@@ -1,5 +1,6 @@
 // loadMap.js
 const script = document.createElement('script');
-script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&callback=initMap`;
+script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_MAPS_API_KEY}&callback=initMap`;
 script.async = true;
 document.head.appendChild(script);
+
